e9d1dd090cf1e6f09c1a1d78492ccf6e138dceec
DEVESHYADAVji <devesh.yadav_cs18@gla.ac.in>
2020-03-31 14:59:11 +0700

    created MyPriorityQueue class of problem 3.

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,43 +1,46 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 23-Mar-20
+ *  Time: 7:15 PM
+ */
 package problem3.myqueue;
-
 
 import problem3.node.Node;
 
 public class MyPriorityQueue {
 
-    private Node tmp, front, end;
-    private int i = 0;
+    private Node front;
 
+    public Node getFront() {
+        return front;
+    }
 
-    //inserting  new student into into queue  and priority is set by roll number
-    public void enqueue(Node newNode) {
+    public void setFront(Node front) {
+        this.front = front;
+    }
 
-        if (front == null || newNode.getS().getRollno() < front.getS().getRollno()) {
-            newNode.setNext(front);
-            front = newNode;
+    public void enQueue(Node newNode) {
+        Node temp = getFront();
+        if (getFront() == null || getFront().getStudent().getRollNumber() < newNode.getStudent().getRollNumber()) {
+            setFront(newNode);
+            getFront().setNextNode(temp);
         } else {
-            tmp = front;
-
-            while (tmp.getNext() != null && tmp.getNext().getS().getRollno() < newNode.getS().getRollno()) {
-                tmp = tmp.getNext();
+            while (temp.getNextNode() != null && temp.getNextNode().getStudent().getRollNumber() <= newNode.getStudent().getRollNumber()) {
+                temp = temp.getNextNode();
             }
-            newNode.setNext(tmp.getNext());
-            tmp.setNext(newNode);
+            newNode.setNextNode(temp.getNextNode());
+            temp.setNextNode(newNode);
         }
     }
 
-    // printing queue
-    public void dequeue() {
-        if (front == null) {
-            System.out.println("No entry found");
-            return;
+    public Node deQueue() {
+        if (getFront() == null) {
+            System.out.println("Empty queue");
+            return null;
         }
-        do {
-            System.out.println(i++);
-            System.out.print(front.getS().getName() + ":");
-            System.out.println(front.getS().getRollno());
-            front = front.getNext();
-        }
-        while (front != null);
+        Node temp = getFront();
+        setFront(getFront().getNextNode());
+        return temp;
     }
 }
